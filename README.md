796eb48fcb860eba6ed5c0714619c098c3c3be6d
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-07-01 18:18:25 +0900

    lesson25 TheReactRouter

---
+++ README.md
@@ -0,0 +1,10 @@
+# React-Router-tut
+
+[Complete React Tutorial (& Redux) #25 - The React Router
+](https://www.youtube.com/watch?v=QUz3k2O3ZJU&list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG&index=26&t=0s)
+
+## Router-v5 でリファクタリングしてみる
+
+## 25 - The React Router
+
+-   Route の書き方
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/contact">
+							<Contact />
+						</Route>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
+}
+
+export default App;
