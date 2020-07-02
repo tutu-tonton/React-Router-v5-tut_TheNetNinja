5b114b3ccb0d4fcf787d4dc81c0bf388e13edda4
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-07-02 17:47:56 +0900

    lesson31 Route Params part2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Home = () => {
@@ -16,7 +17,9 @@ const Home = () => {
 			return (
 				<div className="post card" key={post.id}>
 					<div className="card-content">
-						<span className="card-title">{post.title}</span>
+						<Link to={`/${post.id}`}>
+							<span className="card-title">{post.title}</span>
+						</Link>
 						<p>{post.body}</p>
 					</div>
 				</div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function Post() {
 	const { post_id } = useParams();
 	// console.log(post_id);
+	const [post, setPost] = useState();
 
-	return (
-		<div className="container">
-			<h4>{post_id}</h4>
+	useEffect(() => {
+		axios.get(`http://jsonplaceholder.typicode.com/posts/${post_id}`).then((res) => {
+			console.log(res);
+			setPost(res.data);
+		});
+	}, [post_id]);
+
+	const showPost = post ? (
+		<div className="post">
+			<h4 className="center">{post.title}</h4>
+			<p>{post.body}</p>
 		</div>
+	) : (
+		<div className="center">Loading post...</div>
 	);
+
+	return <div className="container">{showPost}</div>;
 }
 
 export default Post;
